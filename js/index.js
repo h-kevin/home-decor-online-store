21ff54eeb9953514fa3418dc9002bb2fd6c12e76
Kevin Haxhi <kevin.haxhi@softup.co>
2022-09-03 16:13:52 +0200

    Implemented common button and replaced old button in the application

--- js/index.js
+++ js/index.js
@@ -1,8 +1,17 @@
 import createNavbar from '../common/navbar.js';
 import createFooter from '../common/footer.js';
 import createSectionHeader from '../common/sectionHeader.js';
+import createButton from '../common/button.js';
 import categoryPreviewHandler from './handlers/categoryPreview.js';
 import { moveLeft, moveRight } from './handlers/newProductsNavArrows.js';
+
+// Select elements
+const topSectionMiddleColumn = document.querySelector('.top-section .middle-col');
+const topSectionRightColumn = document.querySelector('.top-section .right-col');
+const newProductsSection = document.querySelector('.new-products-section');
+const productsByCategorySection = document.querySelector('.products-by-category-section');
+const orderPlacementSection = document.querySelector('.how-to-place-order-section');
+const socialMediaSubSection = document.querySelector('.social-media-subscription-section');
 
 // Prepend navbar on top of the body
 
@@ -16,30 +25,40 @@ document.body.append(footer);
 
 // Add section header
 
-const topSectionMiddleColumn = document.querySelector('.top-section .middle-col');
 topSectionMiddleColumn.prepend(createSectionHeader(
   'CREATE A COZY ATMOSPHERE FOR YOUR HOME WITH US',
   'main-message',
 ));
 
-const newProductsSection = document.querySelector('.new-products-section');
 newProductsSection.prepend(createSectionHeader(
   'NEW PRODUCTS',
 ));
 
-const productsByCategorySection = document.querySelector('.products-by-category-section');
 productsByCategorySection.prepend(createSectionHeader(
   'HARMONY IN THE DETAILS',
 ));
 
-const orderPlacementSection = document.querySelector('.how-to-place-order-section');
 orderPlacementSection.prepend(createSectionHeader(
   'HOW TO PLACE AN ORDER',
 ));
 
-const socialMediaSubSection = document.querySelector('.social-media-subscription-section');
 socialMediaSubSection.prepend(createSectionHeader(
   'SUBSCRIBE TO OUR INSTAGRAM',
+));
+
+// Add common button
+
+topSectionRightColumn.append(createButton(
+  'To the catalogue',
+  'catalogue-shortcut',
+));
+
+newProductsSection.append(createButton(
+  'All products',
+));
+
+productsByCategorySection.append(createButton(
+  'All products',
 ));
 
 // Setting click event handler for new product arrows in mobile view
